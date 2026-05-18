1e43915461e2004c7aca2fbc17785eaff7ae4472
Dave J Nicol <46354316+davejnicol@users.noreply.github.com>
2026-05-17 22:53:23 -0400

    Transaction styling & script displaying transaction list

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const balanceEl = document.getElementById("balance");
+const incomeEl = document.getElementById("income");
+const expensesEl = document.getElementById("expenses");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+transactionFormEl.addEventListener("submit", addTransaction);
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    // Get Form Values
+    const description = descriptionEl.value.trim();
+    const amount = parseFloat(amountEl.value);
+
+    transactions.push({
+        id: Date.now(),
+        description,
+        amount,
+    });
+
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+    
+    updateTransactionList();
+    // updateSummary();
+    
+    transactionFormEl.reset();
+}
+
+function updateTransactionList() {
+    transactionListEl.innerHTML = "";
+    
+    const sortedTransactions = [...transactions].reverse();
+
+    sortedTransactions.forEach((transaction) => {
+        const transactionEl = createTransactionElement(transaction);
+        transactionListEl.appendChild(transactionEl);
+    });
+}
+
+function createTransactionElement(transaction) {
+    const li = document.createElement("li");
+    li.classList.add("transaction");
+    li.dataset.type = transaction.amount > 0 ? "income" : "expense";
+
+    li.innerHTML = `
+        <span>${transaction.description}</span>
+        <span>
+            ${transaction.amount}
+            <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+        </span>
+    `;
+
+    return li;
+}
+// console.log(li);
